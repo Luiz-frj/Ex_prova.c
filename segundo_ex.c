c59581f96b709a59e0dc58208ccb0d1162ab1851
Luiz Francisco <120425443+Luiz-frj@users.noreply.github.com>
2023-04-25 11:27:21 -0300

    Create segundo_ex.c

---
+++ segundo_ex.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+   int calculo(int espetaculo, float precocnv, int calculoqnt);
+main()
+{
+    float precocnv;
+    int espetaculo, calculoqnt;   
+    printf("informe o preço do espetaculo e quanto cada convite sera vendido: \n");
+    scanf("%d%f", &espetaculo, &precocnv);
+
+    printf("***********************************************\n");
+    printf("- O preço do espetaculo foi: %d\n", espetaculo);
+    printf("- Cada convite foi vendido à: %.2f\n", precocnv);
+    printf("- Para ter lucro, é preciso render mais de: %d convites\n", calculo(espetaculo,precocnv,calculoqnt));
+    printf("***********************************************\n");
+
+}
+int calculo(int espetaculo, float precocnv, int calculoqnt)
+{
+    calculoqnt = espetaculo / precocnv; 
+    return calculoqnt;
+}
