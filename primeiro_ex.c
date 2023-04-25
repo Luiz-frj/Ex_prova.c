b8b2918cb3f25fc3f29e3bf8f5f0a4bc99c7a760
Luiz Francisco <120425443+Luiz-frj@users.noreply.github.com>
2023-04-25 10:55:34 -0300

    Create primeiro_ex.c

---
+++ primeiro_ex.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+   double calcularR( double pesoR, double racaoG1, double racaoG2);
+main() 
+{
+   
+   double pesoR, racaoG1, racaoG2, racaoR;
+
+    printf("informe o peso da ração em kilos: \n");
+    scanf("%lf", &pesoR);
+    printf("informe a quantidade de ração para um dos gato: \n");
+    scanf("%lf", &racaoG1);
+    printf("informe a quantidade de ração para o outr gato: \n");
+    scanf("%lf", &racaoG2);
+
+    printf("***********************************************\n");
+    printf("Resultado: \n");
+    printf("- Peso Inicial: %.2lf\n", pesoR);
+    printf("- quantas gramas o primeiro gato recebeu: %.2lf\n", racaoG1);
+    printf("- quantas gramas o outro gato recebeu: %.2lf\n", racaoG2);
+    printf("- restante da ração depois de 5 dias: %.2lf kilos\n", calcularR (pesoR,racaoG1,racaoG2)/1000);
+    printf("***********************************************\n");
+}
+
+double calcularR(double pesoR, double racaoG1, double racaoG2)
+{
+ double racaoR = pesoR * 1000 - (racaoG1 + racaoG2) * 5;
+return racaoR;
+}
