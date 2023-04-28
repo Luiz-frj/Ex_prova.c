772ee5ab85ae2feb6dee92791710b247170e9210
Luiz Francisco <120425443+Luiz-frj@users.noreply.github.com>
2023-04-28 09:58:53 -0300

    Update terceiro.c

--- terceiro.c
+++ terceiro.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
-int calculo ( int diaria, int desconto, double calculoT);
+float calculo ( float diaria, float desconto, float calculoT);
 int main()
 {
-    double Diaria, calculoT;
-    int desconto;
+    float Diaria, desconto, calculoT;
     printf("Informe o valor da diaria: ");
-    scanf("%d", &Diaria);
+    scanf("%f", &Diaria);
     printf("Infome o valor de desconto: ");
     scanf("%f", &desconto);
     printf("***********************************************\n");
     printf("O valor da diaria era de: R$ %.2lf\n",  Diaria);
-    printf("O percentual do desconto foi de: %f\n", desconto);
-    printf("O valor da diaria com o desconto sera de: R$ %.2lf\n", calculo(Diaria,desconto,calculoT));
+    printf("O percentual do desconto foi de: %.f\n porncetos", desconto);
+    printf("O valor da diaria com o desconto sera de: R$ %.2lf\n %%", calculo(Diaria,desconto,calculoT));
     printf("***********************************************\n");
    return 0;
 }
-int calculo ( int diaria, int desconto, double calculoT)
+float calculo ( float diaria, float desconto, float calculoT)
 {
     calculoT = diaria * (desconto/100);
     return calculoT;
