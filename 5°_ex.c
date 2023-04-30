cf0b1ab4fc4cf77e2f841c534e1339f2885856a9
Luiz Francisco <120425443+Luiz-frj@users.noreply.github.com>
2023-04-29 21:35:53 -0300

    Update 5°_ex.c

--- 5°_ex.c
+++ 5°_ex.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-float transformacao(float dolars, float reais);
+float transformacao(float dolars, float reais, float T);
 int main()
 {
-    float Codia,CoD;
+    float Codia,CoD, T;
     printf("Informe a quantidade de Dolares no cofre: ");
     scanf("%f", &CoD);
     printf("Informe a cotação atual: ");
@@ -11,12 +11,12 @@ int main()
     printf("***********************************************\n");
     printf("Você possue: %.2lf Dolares\n", CoD);
     printf("A cotação atual é de: R$ %.2lf\n", Codia);
-    printf("O valor em reais sera de: R$ %.2lf\n", transformacao(Codia,CoD));
+    printf("O valor em reais sera de: R$ %.2lf\n", transformacao(Codia,CoD, T));
     printf("***********************************************\n");
    return 0;
 }
-float transformacao(float CoD, float Codia)
+float transformacao(float CoD, float Codia, float T)
 {
-    CoD * Codia;
-    return CoD;
+  T = CoD * Codia;
+    return T;
 }
